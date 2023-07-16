b94204265186aa449ddc7f9b67896764819921fa
Abdiqanihussien11 <76992317+Abdiqanihussien11@users.noreply.github.com>
2023-07-16 06:46:17 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {FaRecycle} from "react-icons/fa"
+import './App.css';
+
+
+export default function App() {
+  return (
+    <>
+   <div className='flex'>
+    <div className='bg-[#F0F0F0] w-[50%] h-[100vh]'>
+    </div>
+    <div className='flex justify-center items-center h-screen'>
+    </div>
+    </div>
+    
+    </>
+  )
+}
+
+
